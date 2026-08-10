4fe0324ad083eeb6b48952ebb6a54c3025d7fa67
edpwaterindex <edp@waterindex.co.id>
2026-08-10 07:22:52 +0700

    fix: uom code default value

--- export_coretax/models/uom.py
+++ export_coretax/models/uom.py
@@ -5,4 +5,4 @@ class UomUom(models.Model):
     # Gunakan 'product.uom' jika Anda di Odoo versi 11 ke bawah
     _inherit = 'product.uom' 
 
-    l10n_id_coretax_uom_code = fields.Char(string='Coretax UOM Code', default="UM.0024", help="Kode Satuan Ukur resmi untuk Coretax/E-Faktur 4.0")
+    l10n_id_coretax_uom_code = fields.Char(string='Coretax UOM Code', help="Kode Satuan Ukur resmi untuk Coretax/E-Faktur 4.0")
